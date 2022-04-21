e21936f71da3f24abfae9a70d89f376377bfd20d
Vanity <ktkyletran@gmail.com>
2022-04-20 19:38:36 -0500

    dummy data for graph

---
+++ src/dummyData.js
@@ -0,0 +1,50 @@
+export const userData = [
+  {
+    "name": "Jan",
+    "Active Users": 4000,
+  },
+  {
+    "name": "Feb",
+    "Active Users": 3000,
+  },
+  {
+    "name": "Mar",
+    "Active Users": 2000,
+  },
+  {
+    "name": "Apr",
+    "Active Users": 2780,
+  },
+  {
+    "name": "May",
+    "Active Users": 1890,
+  },
+  {
+    "name": "Jun",
+    "Active Users": 6542,
+  },
+  {
+    "name": "Jul",
+    "Active Users": 2390,
+  },
+  {
+    "name": "Aug",
+    "Active Users": 1345,
+  },
+  {
+    "name": "Sep",
+    "Active Users": 2390,
+  },
+  {
+    "name": "Oct",
+    "Active Users": 8634,
+  },
+  {
+    "name": "Nov",
+    "Active Users": 6532,
+  },
+  {
+    "name": "Dec",
+    "Active Users": 3490,
+  }
+];
