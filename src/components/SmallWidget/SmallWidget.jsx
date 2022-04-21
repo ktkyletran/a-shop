5626855e06e6293f4ce4b6a6e45a4930ed874a97
Vanity <ktkyletran@gmail.com>
2022-04-20 19:39:12 -0500

    added widgets

---
+++ src/components/SmallWidget/SmallWidget.jsx
@@ -0,0 +1,87 @@
+import React from 'react'
+import { Visibility } from '@mui/icons-material'
+import './index.css'
+
+const SmallWidget = () => {
+  return (
+    <div className='widgetSm'>
+      <span className="widgetSmTitle">Newly Joined Members</span>
+      <ul className="widgetSmList">
+        <li className="widgetSmallListItem">
+          <img src="https://api.multiavatar.com/Starcrasher.png
+" alt="User" className="widgetSmImg" />
+          <div className="widgetSmUser">
+            <span className="widgetSmUsername">Anna Kemp</span>
+            <span className="widgetSmUserTitle">Front End Developer</span>
+          </div>
+          <button className="widgetSmButton">
+            <Visibility className='widgetSmIcon' />
+            Display
+          </button>
+        </li>
+        <li className="widgetSmallListItem">
+          <img src="https://api.multiavatar.com/Starcrasher.png
+" alt="User" className="widgetSmImg" />
+          <div className="widgetSmUser">
+            <span className="widgetSmUsername">Anna Kemp</span>
+            <span className="widgetSmUserTitle">Front End Developer</span>
+          </div>
+          <button className="widgetSmButton">
+            <Visibility className='widgetSmIcon' />
+            Display
+          </button>
+        </li>
+        <li className="widgetSmallListItem">
+          <img src="https://api.multiavatar.com/Starcrasher.png
+" alt="User" className="widgetSmImg" />
+          <div className="widgetSmUser">
+            <span className="widgetSmUsername">Anna Kemp</span>
+            <span className="widgetSmUserTitle">Front End Developer</span>
+          </div>
+          <button className="widgetSmButton">
+            <Visibility className='widgetSmIcon' />
+            Display
+          </button>
+        </li>
+        <li className="widgetSmallListItem">
+          <img src="https://api.multiavatar.com/Starcrasher.png
+" alt="User" className="widgetSmImg" />
+          <div className="widgetSmUser">
+            <span className="widgetSmUsername">Anna Kemp</span>
+            <span className="widgetSmUserTitle">Front End Developer</span>
+          </div>
+          <button className="widgetSmButton">
+            <Visibility className='widgetSmIcon' />
+            Display
+          </button>
+        </li>
+        <li className="widgetSmallListItem">
+          <img src="https://api.multiavatar.com/Starcrasher.png
+" alt="User" className="widgetSmImg" />
+          <div className="widgetSmUser">
+            <span className="widgetSmUsername">Anna Kemp</span>
+            <span className="widgetSmUserTitle">Front End Developer</span>
+          </div>
+          <button className="widgetSmButton">
+            <Visibility className='widgetSmIcon' />
+            Display
+          </button>
+        </li>
+        <li className="widgetSmallListItem">
+          <img src="https://api.multiavatar.com/Starcrasher.png
+" alt="User" className="widgetSmImg" />
+          <div className="widgetSmUser">
+            <span className="widgetSmUsername">Anna Kemp</span>
+            <span className="widgetSmUserTitle">Front End Developer</span>
+          </div>
+          <button className="widgetSmButton">
+            <Visibility className='widgetSmIcon' />
+            Display
+          </button>
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+export default SmallWidget
