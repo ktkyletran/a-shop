078e95b037ddf7974ef10aad61e2e8720f4f1fe7
Vanity <ktkyletran@gmail.com>
2022-04-20 19:39:31 -0500

    updated home page with graphs and widgets

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
+import Chart from '../../components/Chart/Chart'
 import FeaturedInfo from '../../components/FeaturedInfo/FeaturedInfo'
+import { userData } from '../../dummyData'
+import SmallWidget from '../../components/SmallWidget/SmallWidget'
+import LargeWidget from '../../components/LargeWidget/LargeWidget'
 import './index.css'
 
 const Home = () => {
   return (
     <div className='home'>
       <FeaturedInfo />
+      <Chart data={userData} title="User Analytics" grid dataKey="Active Users"/>
+      <div className="homeWidget">
+        <SmallWidget />
+        <LargeWidget />
+      </div>
     </div>
   )
 }
