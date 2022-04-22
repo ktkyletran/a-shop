e1797b995f8f5d72fd3fd0673f682e2925f21a5a
Vanity <ktkyletran@gmail.com>
2022-04-21 19:49:13 -0500

    added router and routes

--- src/App.js
+++ src/App.js
@@ -2,17 +2,25 @@ import React from 'react';
 import TopBar from './components/TopBar/TopBar';
 import SideBar from './components/SideBar/SideBar';
 import Home from './pages/Home/Home';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import UserList from './pages/UserList/UserList';
+
 import './App.css'
+import User from './pages/User/User';
 
 function App() {
   return (
-    <div>
+    <Router>
       <TopBar />
       <div className="container">
         <SideBar />
-        <Home />
+        <Routes>
+          <Route exact path="/" element={<Home />}/>
+          <Route path="/users" element={<UserList />} />
+          <Route path="/user/:userId" element={<User />} />
+        </Routes>
       </div>
-    </div>
+    </Router>
   );
 }
 
