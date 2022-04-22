cd35ceefec540bcb8ff6e85f73fc48500cdc33d8
Vanity <ktkyletran@gmail.com>
2022-04-21 19:48:33 -0500

    updated data

--- src/dummyData.js
+++ src/dummyData.js
@@ -48,3 +48,12 @@ export const userData = [
     "Active Users": 3490,
   }
 ];
+
+export const userRows = [
+  { id: 1, userName: 'Jon R.', avatar: 'https://picsum.photos/id/436/200', email: 'jon@gmail.com', status: 'active', transaction: '$2464.00' },
+  { id: 2, userName: 'Kylo T.', avatar: 'https://picsum.photos/id/234/200', email: 'kylo@gmail.com', status: 'active', transaction: '$342.00' },
+  { id: 3, userName: 'Abbey S.', avatar: 'https://picsum.photos/id/543/200', email: 'abb@gmail.com', status: 'active', transaction: '$7452.00' },
+  { id: 4, userName: 'Kyle T.', avatar: 'https://picsum.photos/id/741/200', email: 'kyl@gmail.com', status: 'active', transaction: '$2354.00' },
+  { id: 5, userName: 'Matt H', avatar: 'https://picsum.photos/id/235/200', email: 'matt@gmail.com', status: 'active', transaction: '$6754.00' },
+  { id: 6, userName: 'Chloe B.', avatar: 'https://picsum.photos/id/143/200', email: 'chlo@gmail.com', status: 'active', transaction: '$2341.00' },
+];
